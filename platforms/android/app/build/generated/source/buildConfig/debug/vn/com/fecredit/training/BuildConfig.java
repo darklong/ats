d6e897b2e80ee0e620bab810b3b72bf7c14b0a51
DANG LONG <dvvlong1990@gmail.com>
2018-08-14 07:16:25 +0700

    ok

---
+++ platforms/android/app/build/generated/source/buildConfig/debug/vn/com/fecredit/training/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package vn.com.fecredit.training;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "vn.com.fecredit.training";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 30500;
+  public static final String VERSION_NAME = "3.5.0";
+}
